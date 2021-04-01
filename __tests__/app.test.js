cfd3ff341ce2b0b68f41e682e149627d72974b43
patrick hrabos <phrabos@gmail.com>
2021-03-31 22:18:58 -0400

    add get route to return all races

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,14 +2,16 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const RaceService = require('../lib/services/RaceService');
 
 describe('postgreSQL-vs routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  // beforeEach(() => {
-    
-  // });
+  beforeEach(() => {
+    RaceService.addRace('Pinellas Trail Challenge', 'St. Pete, FL', 46)
+    RaceService.addRace('Daytona 100', 'Daytona Beach, FL', 100)
+  });
 
   it('creates a new ultramarathon in the table', async() => {
     const data = await request(app)
@@ -24,5 +26,25 @@ describe('postgreSQL-vs routes', () => {
       location: 'Death Valley',
       distance: 135
     })
+  })
+
+  it('gets all ultramarathons from the table', async() => {
+    const data = await request(app)
+      .get('/api/v1/races')
+
+    expect(data.body).toEqual([
+      {
+      id: expect.any(String),
+      name: expect.any(String),
+      location: expect.any(String),
+      distance: expect.any(Number)
+      },
+      {
+        id: expect.any(String),
+        name: expect.any(String),
+        location: expect.any(String),
+        distance: expect.any(Number)
+      },
+      ])
   })
 });
